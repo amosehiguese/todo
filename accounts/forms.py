2d22946593fd98059061ada650a2a60554334180
unknown <amosehiguese@gmail.com>
2023-01-11 21:02:22 +0100

    Modify forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,9 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password", help_text="Enter at least 8 characters")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Re-enter password", help_text=False)
+
     email = forms.EmailField()
 
     class Meta:
